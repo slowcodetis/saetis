f4470fc883fb409f09bb0da7e630b5cec6bad46b
rod8 <rivera.rodrigo08@gmail.com>
2015-06-02 23:40:05 -0400

    Cambios Menu
    Cambios Menu

--- Vista/inicio_asesor.php
+++ Vista/inicio_asesor.php
@@ -61,6 +61,13 @@
     <!-- SB Admin CSS - Include with every page -->
     <link href="../Librerias/css/sb-admin.css" rel="stylesheet">
 
+    <style>
+        .menuScroll {
+            overflow: auto;
+            max-height: 500%;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -107,7 +114,7 @@
         </ul>
         <!-- /.navbar-top-links -->
 
-        <div class="navbar-default navbar-static-side" role="navigation">
+        <div class="navbar-default navbar-static-side menuScroll" role="navigation">
             <div class="sidebar-collapse">
                 <ul class="nav" id="side-menu">
                     <li>
