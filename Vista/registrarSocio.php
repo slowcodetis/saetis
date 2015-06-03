b73756b193af52509c0e4148dda3af258090cd7d
Spitpat <san.quit@hotmail.com>
2015-06-03 09:32:40 -0400

    Control de registro de socios por gestion
    DONE

--- Vista/registrarSocio.php
+++ Vista/registrarSocio.php
@@ -12,44 +12,63 @@ include '../Controlador/filtroXSS.php';
     include '../Modelo/conexion.php';
     $data_mysql = new datosmysql();
     $conect = new conexion();
-    $db = $data_mysql->getDB();
-    $host = $data_mysql->getHos();
-    $user = $data_mysql->getUs();
-    $pass = $data_mysql->getPas();
-    $conn = new PDO("mysql:dbname=".$db.";host=".$host,$user, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // iniciar transacción
-    $sqlControl = "SELECT count(*) FROM socio WHERE NOMBRE_U= '$nombreU'";
-    //$valido = $validador -> fetchAll();
-    foreach ($conn -> query($sqlControl) as $valor) {
-        $cantidad = $valor[0];   
-    }
-        
-        //echo"<script type=\"text/javascript\">alert('bark '); window.location='AnadirSocio.php';</script>";
-        if($cantidad < 5) {
-        //SELECT count(*) FROM `socio` WHERE `NOMBRE_U` = 'FreeValue' 
-        $conn->beginTransaction();
-        try {
+    $seleccion="SELECT id_g "
+                    . "FROM gestion "
+                    . "WHERE DATE (NOW()) > DATE(FECHA_INICIO_G) and DATE(NOW()) < DATE(FECHA_FIN_G)";
+    $consulta=$conect->consulta($seleccion);
+    $idGestion=mysql_fetch_row($consulta);
+    $idGestion_= $idGestion[0];
 
-            $sql = 'INSERT INTO socio (NOMBRE_U, NOMBRES_S, APELLIDOS_S) VALUES (:nombreU, :nombreS, :apellidoS);';
+    $controlPorGestion= "SELECT count(*) " 
+                       ."FROM socio "
+                       ."WHERE NOMBRES_S = '$nombS' AND APELLIDOS_S= '$apellS' AND GESTION= $idGestion_";
+    $control = $conect->consulta($controlPorGestion);
+    $registroValido = mysql_fetch_row($control);
+    $rValido = $registroValido[0];
+    if($rValido == 0){
 
-            $result = $conn->prepare($sql);
-
-            $result->bindValue(':nombreU', $nombreU, PDO::PARAM_STR);
-            $result->bindValue(':nombreS', $nombreS, PDO::PARAM_STR);
-            $result->bindValue(':apellidoS', $apellidoS, PDO::PARAM_STR);
-
-            $result->execute();
-
-
-            $conn->commit();
-            echo"<script type=\"text/javascript\">alert('El registro ha sido satisfactorio'); window.location='AnadirSocio.php';</script>";
-        } catch (PDOException $e) {
-           // si ocurre un error hacemos rollback para anular todos los insert
-            $conn->rollback();
-            echo $e->getMessage();
+        $db = $data_mysql->getDB();
+        $host = $data_mysql->getHos();
+        $user = $data_mysql->getUs();
+        $pass = $data_mysql->getPas();
+        $conn = new PDO("mysql:dbname=".$db.";host=".$host,$user, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // iniciar transacción
+        $sqlControl = "SELECT count(*) FROM socio WHERE NOMBRE_U= '$nombreU'";
+        //$valido = $validador -> fetchAll();
+        foreach ($conn -> query($sqlControl) as $valor) {
+            $cantidad = $valor[0];   
         }
-        } else {
-            echo"<script type=\"text/javascript\">alert('El registro no fue realizado debido a que la empresa tiene el maximo numero de socios registrados'); window.location='AnadirSocio.php';</script>";
-        }  
+            
+            //echo"<script type=\"text/javascript\">alert('bark '); window.location='AnadirSocio.php';</script>";
+            if($cantidad < 5) {
+            //SELECT count(*) FROM `socio` WHERE `NOMBRE_U` = 'FreeValue' 
+            $conn->beginTransaction();
+            try {
+
+                $sql = 'INSERT INTO socio (NOMBRE_U, NOMBRES_S, APELLIDOS_S, GESTION) VALUES (:nombreU, :nombreS, :apellidoS, :idGestion_);';
+
+                $result = $conn->prepare($sql);
+
+                $result->bindValue(':nombreU', $nombreU, PDO::PARAM_STR);
+                $result->bindValue(':nombreS', $nombreS, PDO::PARAM_STR);
+                $result->bindValue(':apellidoS', $apellidoS, PDO::PARAM_STR);
+                $result->bindValue(':idGestion_', $idGestion_, PDO::PARAM_STR);
+
+                $result->execute();
+
+
+                $conn->commit();
+                echo"<script type=\"text/javascript\">alert('El registro ha sido satisfactorio'); window.location='AnadirSocio.php';</script>";
+            } catch (PDOException $e) {
+               // si ocurre un error hacemos rollback para anular todos los insert
+                $conn->rollback();
+                echo $e->getMessage();
+            }
+            } else {
+                echo"<script type=\"text/javascript\">alert('El registro no fue realizado debido a que la empresa tiene el maximo numero de socios registrados'); window.location='AnadirSocio.php';</script>";
+            } 
+    } else {
+        echo"<script type=\"text/javascript\">alert('El socio que intenta registrar ya se encuentra registrado en una grupo empresa para la presente gestion'); window.history.back();</script>";
+    } 
     ?>
