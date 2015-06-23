8d7a79c359de96a04cfd43b2c1dd62f611edb738
crhystamils <crhystamil@gmail.com>
2015-06-23 19:35:28 -0400

    [user, pass DB]

--- Vista/configDB.php
+++ Vista/configDB.php
@@ -1,17 +1,15 @@
 <?php
 class datosmysql{
-/*    
 public $usernameDB = 'adminTpQmknN';
 public $passwordDB = '77g4ePielXPg';
 public $hostDB = '127.5.49.2';
 public $nameDB = 'saetis';
-/*/
+/*
     public $usernameDB = 'root';
     public $passwordDB = 'Crhyst23';
     public $hostDB = 'localhost';
     public $nameDB = 'saetis';
- //*/
-
+ */
     function getUs(){
         return $this->usernameDB;
     }
